5e3630b338b97f4e0fc73f269edcd0f7db9c81de
MDmitryM <makarovdmitryd@yandex.ru>
2022-10-09 15:33:59 +0300

    lab4 done 50%

--- lab4/app/src/main/java/com/example/lab21/LoginActivity2.kt
+++ lab4/app/src/main/java/com/example/lab21/LoginActivity2.kt
@@ -1,17 +1,28 @@
 package com.example.lab21
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 //import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.util.Log
 //import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login2.*
 
+
+
+
+
 class LoginActivity2 : AppCompatActivity()
 {
+    private val PREF_NAME = "LoginActivityPref"
+    private val EMAIL_KEY:String = "e"
+    private val PASSWORD_KEY:String = "p"
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -34,19 +45,21 @@ class LoginActivity2 : AppCompatActivity()
                         strPass = PasswordText.text.toString()
 
 
-                        intent.putExtra("e", strEmail)
-                        intent.putExtra("p",strPass)
+                        intent.putExtra(EMAIL_KEY, strEmail)
+                        intent.putExtra(PASSWORD_KEY,strPass)
 
                         startActivity(intent)
                     }
                     else
                     {
-                        Toast.makeText(this,"Invalid password", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this,"Invalid password", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
                 else
                 {
-                    Toast.makeText(this,"Invalid E-mail", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this,"Invalid E-mail", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
         }
@@ -69,12 +82,47 @@ class LoginActivity2 : AppCompatActivity()
     {
         Log.i("AppLogger", "onResumeAct2")
         super.onResume()
+
+        getPrefData()
+    }
+    private fun getPrefData()
+    {
+        val sharedPreferences = getSharedPreferences(PREF_NAME,
+            Context.MODE_PRIVATE)
+
+        val strSharedPreferencesEmail = sharedPreferences.
+            getString(EMAIL_KEY,"")
+
+        val strSharedPreferencesPassword = sharedPreferences.
+        getString(PASSWORD_KEY,"")
+
+        EmailText.setText(strSharedPreferencesEmail)
+        PasswordText.setText(strSharedPreferencesPassword)
+
+        Toast.makeText(this,"Data Restored!",Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause()
     {
         Log.i("AppLogger", "onPauseAct2")
         super.onPause()
+
+        savePrefData()
+    }
+    private fun savePrefData()
+    {
+        val insertEmail = EmailText.text.toString()
+        val insertPassword = PasswordText.text.toString()
+
+        val sharedPreferences = getSharedPreferences(PREF_NAME,
+            Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(EMAIL_KEY,insertEmail)
+        editor.putString(PASSWORD_KEY,insertPassword)
+        editor.apply()
+
+        Toast.makeText(this,"Data Saved",Toast.LENGTH_SHORT)
+            .show()
     }
 
     override fun onStop()
