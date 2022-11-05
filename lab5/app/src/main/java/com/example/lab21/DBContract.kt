5525493ffd343bd4f52c2e0671767525dc49f513
MDmitryM <68000462+MDmitryM@users.noreply.github.com>
2022-11-05 14:46:18 +0300

    almost done lab5

--- lab5/app/src/main/java/com/example/lab21/DBContract.kt
+++ lab5/app/src/main/java/com/example/lab21/DBContract.kt
@@ -7,16 +7,15 @@ object DBContract
     // Table contents are grouped together in an anonymous object.
     object UserEntry : BaseColumns {
         const val TABLE_NAME = "users"
-        const val COLUMN_NAME_KEY_ID = "id"
-        const val COLUMN_NAME_LOGIN = "login"
+        //const val COLUMN_NAME_KEY_ID = "id"
+        const val COLUMN_NAME_EMAIL = "email"
         const val COLUMN_NAME_PASSWORD = "password"
     }
 
     private const val SQL_CREATE_ENTRIES =
         "CREATE TABLE ${UserEntry.TABLE_NAME} (" +
                 "${BaseColumns._ID} INTEGER PRIMARY KEY," +
-                "${UserEntry.COLUMN_NAME_KEY_ID} TEXT," +
-                "${UserEntry.COLUMN_NAME_LOGIN} TEXT" +
+                "${UserEntry.COLUMN_NAME_EMAIL} TEXT," +
                 "${UserEntry.COLUMN_NAME_PASSWORD} TEXT)"
 
     private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${UserEntry.TABLE_NAME}"
